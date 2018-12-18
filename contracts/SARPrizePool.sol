e1cc8810c4bbaeaafb540e5649dc4a84064ddca3
Anakorn Kyavatanakij <anakornk@gmail.com>
2018-12-18 20:01:49 +0800

    changed visibility for state variables

--- contracts/SARPrizePool.sol
+++ contracts/SARPrizePool.sol
@@ -2,16 +2,17 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract SARPrizePool {
 
-    address public owner;
-    uint public winnersCount;
-    uint public frozenPrizePerShare;
-    bool public isFrozen;
+    address private owner;
+    uint private winnersCount;
+    uint private frozenPrizePerShare;
+    bool private frozen;
 
-    uint internal C;
-    mapping ( address => bool ) hasClaimed;
-    mapping ( address => bool ) isWinner;
+    uint private C;
+    mapping ( address => bool ) private hasClaimed;
+    mapping ( address => bool ) private winnerMap;
 
-    event ClaimPrize(address user, uint amount);
+    event ClaimPrize(address winner, uint amount);
+    event NewWinner(address winner);
 
     constructor(uint _C) public {
         C = _C;
@@ -21,9 +22,8 @@ contract SARPrizePool {
     // Transfer Prize to Msg.sender
     function _transferPrize() internal;
 
-    function claimPrize() public {
-        require(isFrozen, "Winners list is not frozen");
-        require(isWinner[msg.sender], "You're not a winner");
+    function claimPrize() public isFrozen {
+        require(winnerMap[msg.sender], "You're not a winner");
         require(hasClaimed[msg.sender] == false, "You have claimed the prize already");
         hasClaimed[msg.sender] = true;
         _transferPrize();
@@ -40,12 +40,52 @@ contract SARPrizePool {
         return winnersCount * getCurrentPrizePerShare();
     }
 
-    function canFreeze() internal returns (bool);
+    function canFreeze() internal view returns (bool);
 
-    function freeze() public {
+    function freeze() public notFrozen {
         require(canFreeze(), "No permmission to freeze");
-        isFrozen = true;
+        frozen = true;
         frozenPrizePerShare = getCurrentPrizePerShare();
+    }
+
+    modifier isFrozen() {
+        require(frozen, "Not frozen");
+        _;
+    }
+
+    modifier notFrozen() {
+        require(!frozen, "Frozen");
+        _;
+    }
+
+    function addWinner(address winner) internal notFrozen {
+        uint temp = winnersCount + 1;
+        require(temp >= winnersCount, "Overflow");
+        winnersCount = temp;
+        winnerMap[winner] = true;
+        emit NewWinner(winner);
+    }
+
+    function getFrozenPrizePerShare() public view isFrozen returns (uint) {
+        return frozenPrizePerShare;
+    }
+
+    function getFrozenPrizePool() public view isFrozen returns (uint) {
+        return winnersCount * frozenPrizePerShare;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+
+    modifier isWinner {
+        require(winnerMap[msg.sender], "You're not a winner!");
+        _;    
+    }
+
+    modifier isNotWinner {
+        require(!winnerMap[msg.sender], "You're already a winner!");
+        _;
     }
 }
 
@@ -63,37 +103,24 @@ contract ETHPrizePool is SARPrizePool {
 
     // Transfer prize to msg.sender
     function _transferPrize() internal {
-        msg.sender.transfer(frozenPrizePerShare);
+        msg.sender.transfer(getFrozenPrizePerShare());
     }
 }
 
 contract SimpleSumGame is ETHPrizePool {
     uint public sum;
 
-    event SubmitAnswer(address user, uint a, uint b, bool correct);
-
     constructor(uint _sum) public ETHPrizePool(1) {
         sum = _sum;
     }
 
-    modifier isNotWinner {
-        require(!isWinner[msg.sender], "You're already a winner!");
-        _;
-    }
-
     function submitAnswer(uint _a, uint _b) public isNotWinner {
         if (_a + _b == sum) {
-            uint temp = winnersCount + 1;
-            require(temp >= winnersCount, "Overflow");
-            winnersCount = temp;
-            isWinner[msg.sender] = true;
-            emit SubmitAnswer(msg.sender, _a, _b, true);
-        }  else {
-            emit SubmitAnswer(msg.sender, _a, _b, false);
-        }
+            addWinner(msg.sender);
+        } 
     }
 
-    function canFreeze() internal returns (bool) {
-        return msg.sender == owner;
+    function canFreeze() internal view returns (bool) {
+        return msg.sender == getOwner();
     }
 }
